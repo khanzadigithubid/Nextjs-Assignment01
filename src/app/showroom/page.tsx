4f2cd76cac3b56a09cdbfb50224fa157261746c0
khanzadigithubid <memonbisma49@gmail.com>
2024-10-08 23:49:21 +0500

    Changes For Improvement

--- src/app/showroom/page.tsx
+++ src/app/showroom/page.tsx
@@ -1,11 +1,10 @@
-// app/showroom/page.tsx
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/Showroom.module.css';
-import car1 from '../public/image/car1.jpg'; 
+import car1 from '../public/image/car1.jpg';
 import car2 from '../public/image/car2.jpg';
 import car3 from '../public/image/car3.jpg';
-import car4 from '../public/image/car4.jpg'; // Add new car images
+import car4 from '../public/image/car4.jpg';
 import car5 from '../public/image/car5.jpg';
 import car6 from '../public/image/car6.jpg';
 
@@ -13,9 +12,9 @@ const cars = [
   { id: '1', name: 'Corolla Car', image: car1 },
   { id: '2', name: 'Civic Car', image: car2 },
   { id: '3', name: 'Haval Car', image: car3 },
-  { id: '4', name: 'Tayto Car', image: car4 }, // New car
-  { id: '5', name: 'Sports Car', image: car5 }, // New car
-  { id: '6', name: 'Electric Car', image: car6 }, // New car
+  { id: '4', name: 'Tayto Car', image: car4 },
+  { id: '5', name: 'Sports Car', image: car5 },
+  { id: '6', name: 'Electric Car', image: car6 },
 ];
 
 const Showroom: React.FC = () => {
@@ -25,18 +24,17 @@ const Showroom: React.FC = () => {
       <div className={styles.carList}>
         {cars.map((car) => (
           <div key={car.id} className={styles.carCard}>
-            <Link href={`/showroom/${car.id}`} passHref className={styles.imageWrapper}>
-              <div>
-                <Image 
-                  src={car.image} 
-                  alt={car.name} 
-                  layout="responsive"
-                  width={300}
-                  height={200}
-                  className={styles.carImage}
-                />
-                <h2 className={styles.carName}>{car.name}</h2>
-              </div>
+            <Image 
+              src={car.image} 
+              alt={car.name} 
+              layout="responsive"
+              width={300}
+              height={200}
+              className={styles.carImage}
+            />
+            <h2 className={styles.carName}>{car.name}</h2>
+            <Link href={`/showroom/${car.id}`} passHref>
+              <button className={styles.detailsButton}>View Details</button>
             </Link>
           </div>
         ))}
